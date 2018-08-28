47fcbda791bddb218c3364011c9bb8236fabddaf
Hamze <hamzeasadi1@gmail.com>
2018-08-28 17:58:02 +0430

    my first cellular automata code

---
+++ cellular_automato.py
@@ -0,0 +1,17 @@
+import pyglet
+
+class Window(pyglet.window.Window):
+	
+	def __init__(self):
+		super().__init__()
+		self.set_size(600,600)
+
+	def on_draw(self):
+		self.clear()
+		pyglet.graphics.draw_indexed(3, pyglet.gl.GL_TRIANGLES, [0, 1, 2], ('v2i', (300, 300, 300, 350, 350, 300)))
+
+if __name__ == '__main__':
+	window = Window()
+	pyglet.app.run()
+
+	
